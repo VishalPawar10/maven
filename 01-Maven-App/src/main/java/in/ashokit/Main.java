540afa549805be1121b11c48a1cf1b978cb8afeb
Vishal <vishalsureshpawar@gmail.com>
2024-08-04 10:23:19 +0530

    pom modified

--- 01-Maven-App/src/main/java/in/ashokit/Main.java
+++ 01-Maven-App/src/main/java/in/ashokit/Main.java
@@ -2,6 +2,7 @@ package in.ashokit;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//dummy change
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
